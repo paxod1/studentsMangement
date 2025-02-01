d4dd34cc96ecb8286d28ba90f48a0f1587cd0482
chiku binoy <chikubinoybinoy@gmail.com>
2025-02-01 17:56:47 +0530

    some changes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import ReactDOM from "react-dom";  // Use 'react-dom' for React 16
+import ReactDOM from 'react-dom/client';  // Correct import for React 18
 import App from "./App.jsx";
 import "./index.css";
 import { persistor, store } from "./Redux/store.js";
@@ -7,13 +7,14 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));  // Create root for React 18
+
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
