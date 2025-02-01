2f63a1eaebe4f1c3b1d2dff88af3f1c906082611
chiku binoy <chikubinoybinoy@gmail.com>
2025-02-01 14:53:08 +0530

    some changes

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { persistor, store } from './Redux/store.js'
-import { Provider } from 'react-redux'
-import { PersistGate } from 'redux-persist/integration/react'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from "react";
+import ReactDOM from "react-dom";  // Use 'react-dom' for React 16
+import App from "./App.jsx";
+import "./index.css";
+import { persistor, store } from "./Redux/store.js";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
@@ -15,4 +15,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       </PersistGate>
     </Provider>
   </React.StrictMode>,
-)
+  document.getElementById('root')
+);
