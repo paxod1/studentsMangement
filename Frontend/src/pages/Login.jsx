d5fddcf651573149d65d2d7f757725ce4adda702
chiku binoy <chikubinoybinoy@gmail.com>
2025-02-01 20:39:32 +0530

    some changes

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,50 +1,52 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
 import './login.css';
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { loginUser } from '../API/ApiCalling';
-import { useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-
+import { useDispatch, useSelector } from 'react-redux';
 
 function Login() {
-  const [username, setUsername] = useState()
-  const [password, setPassword] = useState()
-  const dispatch = useDispatch()
-  const navigate = useNavigate()
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const loginInfo = useSelector((state) => state.userlogin?.LoginInfo); // Access login state from Redux
 
+  useEffect(() => {
+    if (loginInfo?.token) {
+      navigate('/');  
+    }
+  }, [loginInfo, navigate]); 
 
-  async function APIcallLogin() {
-
-    await loginUser({ username, password }, dispatch)
-    navigate('/');
+  async function handleLogin() {
+    const success = await loginUser({ username, password }, dispatch);
+    if (success) {
+      navigate('/');  
+    }
   }
 
   return (
-    <div>
-      <div className="login-container">
-        <div className="login-box">
-          <div className="login-logo">
-            <img src="../../public/logo (1).png" width={'250px'} height={'150px'} alt="" />
-          </div>
-
-          <input
-            className="login-input"
-            type="text"
-            placeholder="email"
-            onChange={((e) => setUsername(e.target.value))}
-          />
-          <input
-            className="login-input"
-            type="password"
-            placeholder="Password"
-            onChange={((e) => setPassword(e.target.value))}
-          />
-          <button className="login-button" onClick={APIcallLogin}>Log In</button>
-
+    <div className="login-container">
+      <div className="login-box">
+        <div className="login-logo">
+          <img src="../../public/logo (1).png" width="250px" height="150px" alt="" />
         </div>
+
+        <input
+          className="login-input"
+          type="text"
+          placeholder="Email"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          className="login-input"
+          type="password"
+          placeholder="Password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="login-button" onClick={handleLogin}>Log In</button>
       </div>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
