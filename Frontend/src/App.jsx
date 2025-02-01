b7c28de0bc678924ed54c4bf49a463e6a900fa02
chiku binoy <chikubinoybinoy@gmail.com>
2025-02-01 20:58:11 +0530

    a

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,38 +1,37 @@
-import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Login from './pages/Login'
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import Login from './pages/Login';
 import Home from './pages/Home';
 import ClassVideo from './pages/ClassVideo';
 
-
 function App() {
-
   const logininfom = useSelector((state) => state.userlogin?.LoginInfo[0]);
   console.log("from app.js logininfom", logininfom);
 
-
-
-
   const token = logininfom?.token;
-  
 
+  // Setting up routing paths
   const app = createBrowserRouter([
     {
-      path: "/",
-      element: token ? <Home /> : <Login />
+      path: "/login", // Login page path
+      element: token ? <Home /> : <Login /> // Redirect to Home if already logged in
+    },
+    {
+      path: "/", // Home page path
+      element: token ? <Home /> : <Login /> // Redirect to Login if not logged in
     },
     {
       path: "/ClassVideo",
-      element: token ? <ClassVideo /> : <Login />
-
+      element: token ? <ClassVideo /> : <Login /> // Conditional rendering based on login
     },
-  ])
+  ]);
+
   return (
     <div>
       <RouterProvider router={app} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
