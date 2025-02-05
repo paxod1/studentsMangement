cefc15b03c6e1e13905b8825bd9ee6913a7a61a3
chiku binoy <chikubinoybinoy@gmail.com>
2025-02-05 12:51:45 +0530

    some changes

--- Backend/index.js
+++ Backend/index.js
@@ -8,14 +8,21 @@ dotenv.config();
 
 const app = express();
 
+
 const corsOptions = {
-  origin: 'https://students-mangement.vercel.app/',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
-  optionsSuccessStatus: 200
+  origin: 'https://students-mangement.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE']
 };
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`Request Origin: ${req.headers.origin}`);
+  console.log(`Request Headers: ${JSON.stringify(req.headers)}`);
+  next();
+});
+
 
 
 app.use('/student', studentRouter);
