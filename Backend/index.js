f7d1277f7701c2a7b608de229888c18006084b32
chiku binoy <chikubinoybinoy@gmail.com>
2025-02-05 13:34:18 +0530

    some changes

--- Backend/index.js
+++ Backend/index.js
@@ -3,16 +3,15 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const studentRouter = require('./Routers/studentRouter');
 
-
 dotenv.config();
 
 const app = express();
+
+// Middleware setup
 app.use(cors());
+app.use(express.json()); 
 
-
-
-
-
+// Route setup
 app.use('/student', studentRouter);
 
 const PORT = process.env.PORT || 5000;
