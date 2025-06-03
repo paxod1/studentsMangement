b5bb0780120bbfba8f4707f53a23c91b6ea781e7
chiku binoy <chikubinoybinoy@gmail.com>
2025-06-03 16:38:09 +0530

    some updates

--- Frontend/src/components/HomePoster.jsx
+++ Frontend/src/components/HomePoster.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { Carousel } from 'react-bootstrap';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 import './HomePoster.css';
 
 function HomePoster() {
@@ -13,19 +15,35 @@ function HomePoster() {
     'https://techwingsys.com/banner_7.png'
   ]);
 
+  const settings = {
+    dots: false,              // Hide the navigation dots
+    arrows: false,            // Hide the arrow buttons
+    infinite: true,           // Infinite looping
+    speed: 500,               // Transition speed
+    slidesToShow: 1,          // Show one slide at a time
+    slidesToScroll: 1,        // Scroll one slide at a time
+    autoplay: true,           // Enable auto-play
+    autoplaySpeed: 3000,      // Auto-play speed
+    draggable: true,          // Enable dragging/swiping
+    swipe: true,              // Enable swipe
+    swipeToSlide: true,       // Allow swiping to slide
+    pauseOnHover: false       // Don't pause on hover
+  };
+
   return (
     <div className="home-poster-container">
-      <Carousel fade interval={3000}>
+      <Slider {...settings}>
         {posterData.map((src, index) => (
-          <Carousel.Item key={index}>
+          <div key={index}>
             <img
-              className="d-block poster-img"
+              className="poster-img"
               src={src}
               alt={`Slide ${index + 1}`}
+
             />
-          </Carousel.Item>
+          </div>
         ))}
-      </Carousel>
+      </Slider>
     </div>
   );
 }
