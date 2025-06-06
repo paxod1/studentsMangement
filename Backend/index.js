2c3f42a5e51e0caffef0f5642ee2f1a19b71c9da
chiku binoy <chikubinoybinoy@gmail.com>
2025-06-06 17:10:30 +0530

    some backend updates

--- Backend/index.js
+++ Backend/index.js
@@ -17,6 +17,10 @@ app.use(cors());
 // }
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader('Content-Type', 'application/json; charset=utf-8');
+  next();
+});
 
 // Route setup
 app.use('/student', studentRouter);
